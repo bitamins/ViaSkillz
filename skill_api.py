7e4e54f635a916b8ae222a0519ac8376e27fff79
Mike Sun <msun@lp-b053829-osx.hq.corp.viasat.com>
2019-07-13 22:58:22 -0700

    skillup

--- skill_api.py
+++ skill_api.py
@@ -1,6 +1,7 @@
 import os
 from flask_api import FlaskAPI
 from flask_pymongo import PyMongo
+from flask import request
 
 # client = pymongo.MongoClient('mongodb+srv://mb:mb@viaskillz-runjj.mongodb.net/test?retryWrites=true&w=majority')
 
@@ -17,18 +18,61 @@ def index():
     return text
 
 
-@app.route("/skillup")
+@app.route("/skillup", methods=['POST'])
 def skillup():
+    # Assume user, skills collections exist
     # data = request.args
     
-    test = {
-        'andrew':{
-            'python': 1
-        }
-    }
-    mongo.db.test.insert_one(test)
+    mydata = mongo.db.data
+    # print(flask.request)
+    print("AAAAAAA")
+    print(request.data,type(request.data))
+    text = request.data
+    # args = text.split(' ')
+    myname = text['name']
+    myskill = text['skill']
 
-    return 'test'
+
+    print('name {} skill {}'.format(myname,myskill))
+
+    try:
+        docdict = json.loads(mydata.find({"name": myname}))[0] #Debug this forsure probably
+    except Exception as e:
+        print('query failed with: {}'.format(e))
+        docdict = False
+    if docdict:
+        if myskill in docdict["skills"]:
+            docdict["skills"][myskill] += 1
+        else:
+            docdict["skills"][myskill] = 1
+        mydata.remove({"name": myname})
+    else:
+        docdict = {
+                "name": myname,
+                "skills": {
+                    myskill:1
+                }
+            }
+    
+    mydata.insert_one(docdict)
+
+    # # QUERYING FROM MONGODB
+    # if mydata.find({'name':name}):
+    #     # if name exists
+    #     result = mydata.update_one({name:{skill:}},{})
+    # else:
+    #     # if name not exist yet
+    #     if mydata.find({"skills".skill:})
+    #     result = mydata.insert_one(
+    #     [{   
+    #         "name": myname,
+    #         "skills":[
+    #             {skill:1}
+    #         ]
+    #     }]
+    #     )
+
+    return str(myname)+" "+str(myskill)
 
 
 
@@ -38,6 +82,7 @@ def skillup():
 if __name__ == "__main__":
     # intro = 'Running your app now!'
     # print('Hello: {}'.format(intro))
+    host='0.0.0.0'
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)

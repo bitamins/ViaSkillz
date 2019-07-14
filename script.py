96c32938e8da6ed9ecc9ca40fbbc198170d9921b
Elliot Eisenberg <elliot.eisenberg@viasat.com>
2019-07-13 18:08:22 -0700

    new port

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 from flask import Flask, escape, request
 
 app = Flask(__name__)
+myport = process.env.PORT || 5000
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-app.run(port=8080)
+app.run(port=myport)

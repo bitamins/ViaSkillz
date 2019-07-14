883dfe6b5f7aa9fa86b3cb78466e951d3586bdc8
Elliot Eisenberg <elliot.eisenberg@viasat.com>
2019-07-13 18:43:40 -0700

    Revert "fixed"
    This reverts commit 50f7ba516c2d4ba279673dca8f2eca31a6ff7d00.

--- skill_api.py
+++ skill_api.py
@@ -1,13 +1,9 @@
 from flask_api import FlaskAPI
-from flask_pymongo import PyMongo
+import pymongo
 
-# client = pymongo.MongoClient('mongodb+srv://mb:mb@viaskillz-runjj.mongodb.net/test?retryWrites=true&w=majority')
+client = pymongo.MongoClient('mongodb+srv://mb:mb@viaskillz-runjj.mongodb.net/test?retryWrites=true&w=majority')
 
 app = FlaskAPI(__name__)
-
-app.config['MONGO_URI'] = 'mongodb+srv://mb:mb@viaskillz-runjj.mongodb.net/test?retryWrites=true&w=majority'
-
-mongo = PyMongo(app)
 
 @app.route("/")
 def index():
@@ -16,18 +12,9 @@ def index():
     return text
 
 
-@app.route("/skillup")
+@app.route("/skillup", methods=['GET', 'POST'])
 def skillup():
-    # data = request.args
-    
-    test = {
-        'andrew':{
-            'python': 1
-        }
-    }
-    mongo.db.test.insert_one(test)
-
-    return 'test'
+    data = request.data
 
 
 
